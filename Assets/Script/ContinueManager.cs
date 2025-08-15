6384a58c4f2f2fd13b515aa81a0cc076ba4f34a4
阿久津匠輝 <shoki.akutsu.vantan@gmail.com>
2025-08-15 18:37:05 +0900

    [add] コンテニューのシステム導入前

--- Assets/Script/ContinueManager.cs
+++ Assets/Script/ContinueManager.cs
@@ -6,15 +6,5 @@ public class ContinueManager : MonoBehaviour
 {
     [SerializeField] ContinueCost _cost;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
